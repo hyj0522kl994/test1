fad450a0f9dc6832edcee92074e5145a8d50606e
Yoojin Ha <hyj0522kl994@outlook.com>
2018-02-02 11:02:54 +0900

    electron + chat by using websocket
    chatting function by websocket , still having error to run the main process

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-const electron = require('electron');
-const url= require('url');
-const path = require('path');
+const path = require("path");
 
-const {app, BrowserWindow}= electron;
+const { app, BrowserWindow } = require("electron");
 
-let mainWindow;
+const WebSocket = require("ws")
 
-//listen for the app to be ready
-app.on('ready',function(){
-//create new window
-  mainWindow = new BrowserWindow({});
-  //load html into window
-  mainWindow.loadURL(url.format({
-    pathname:path.join(__dirname,'mainWindow.html'),
-    protocol:'file:',
-    slashes:true
-  }));
+const wss = new WebSocket.Server( { port: 1040 } );
+
+let win;
+function createWindow() {
+  win = new BrowserWindow({ width: 1250, height: 700 });
+  win.loadURL(`file://${__dirname}/mainWindow.html`);
+  win.on("closed", () => { win = null; });
+
+  wss.on('connection', function (w) {
+      w.on( 'message' , function (data)  {
+           console.log(data);
+
+           w.send(data);
+      });
+      w.on('close', function() {
+           console.log("Closed");
+      });
+
+  });
+
+}
+
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (win === null) {
+    createWindow();
+  }
 });
